e95288bc884189d75fb763e446a74e7ad773f73e
3mran <amranabid36@gmail.com>
2025-02-10 21:50:33 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports={
+    probot_ids :'',//أيدي بروبوت
+    recipientId : '',//أيدي الشخص ألي يتحول له الكريدت   
+    price : '',//سعر النسخ
+    serverid : '',//أيدي سيرفر
+    token : ''//توكن بوتك
+}
