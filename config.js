441d2b72c987e8ca4a736fe53bf914f4d8cd868c
snguty <hamotshi.h.habibi@gmail.com>
2025-07-29 23:14:31 +0200

    Update and rename config.js to pakage.json

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports={
-    probot_ids :'',//أيدي بروبوت
-    recipientId : '',//أيدي الشخص ألي يتحول له الكريدت   
-    price : '',//سعر النسخ
-    serverid : '',//أيدي سيرفر
-    token : ''//توكن بوتك
-}
